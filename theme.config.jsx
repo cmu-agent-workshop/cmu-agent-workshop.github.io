804dd58f75a985ff42530ac04b8dca2d6fa73405
Hao <prokilchu@gmail.com>
2024-04-15 17:15:35 -0400

    add og

--- theme.config.jsx
+++ theme.config.jsx
@@ -31,6 +31,13 @@ export default {
           return {
             titleTemplate: '%s – Agent Workshop @ CMU',
             description: 'Agent Workshop 2024 brings together researchers and practitioners in the field of AI. The workshop will be held at Carnegie Mellon University, Pittsburgh, PA, USA.',
+            openGraph: {
+                type: 'website',
+                locale: 'en_US',
+                url: 'https://cmu-agent-workshop.github.io/',
+                siteName: 'Agent Workshop @ CMU',
+                description: 'Agent Workshop 2024 brings together researchers and practitioners in the field of AI. The workshop will be held at Carnegie Mellon University, Pittsburgh, PA, USA.',
+            }
           }
         }
     },
