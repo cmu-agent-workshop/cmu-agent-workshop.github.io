3cbf89bc10b29302d5a9ba7995e3d3073698cb10
Hao <prokilchu@gmail.com>
2024-04-23 09:34:35 -0400

    chore: Update opengraph image for better social media sharing

--- theme.config.jsx
+++ theme.config.jsx
@@ -40,7 +40,11 @@ export default {
         description: 'Agent Workshop 2024 brings together researchers and practitioners in the field of AI. The workshop will be held at Carnegie Mellon University, Pittsburgh, PA, USA.',
         openGraph: {
             type: 'website',
-            image: 'https://cmu-agent-workshop.github.io/cover.jpg',
+            images: [
+              {
+                url: 'https://cmu-agent-workshop.github.io/cover.jpg',
+              }
+            ],
             locale: 'en_US',
             url: 'https://cmu-agent-workshop.github.io/',
             siteName: 'Agent Workshop @ CMU',
